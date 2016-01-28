29d94020c1034ae38221a17ec31826e3360db340
Thomas Gummerer <t.gummerer@gmail.com>
2016-01-28 15:40:32 +0100

    print revision count

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate git2;
+
+use git2::Repository;
+
 fn main() {
-    println!("Hello, world!");
+    let repo = match Repository::open (".") {
+        Ok(repo) => repo,
+        Err(_) => panic!("you have to be inside of a git repository to use git-submit"),
+    };
+    match repo.revwalk() {
+        Ok(revwalk) => println!("{}", revwalk.count()),
+        Err(_) => panic!("can't create revwalk"),
+    };
 }
